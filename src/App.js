f3557ad4d59e1121753bba6a9311a084ab35220c
Melvin <peepersm@gmail.com>
2021-03-02 23:21:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deploying React app to Netlify
         </a>
       </header>
     </div>
