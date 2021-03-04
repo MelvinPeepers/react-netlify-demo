8b1af6df25ba5fd2b40016ad1846be903e557dee
Melvin <peepersm@gmail.com>
2021-03-03 19:07:06 -0500

    Testing Netlify Dev

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deploying React app to Netlify
+          Using Netlify Dev
         </a>
       </header>
     </div>
